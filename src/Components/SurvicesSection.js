e3bcb9c7fed710ac172611beacf1530eac1906ad
Pavel Staykov <pavkata.staikov@gmail.com>
2021-01-24 19:44:26 +0200

    setUp

---
+++ src/Components/SurvicesSection.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+//Import Icons
+import clock from "../img/clock.svg";
+import diaphragm from "../img/diaphragm.svg"
+import money from "../img/money.svg"
+import teamwork from "../img/teamwork.svg"
+
+
+const SurvicesSection = () =>{
+    return(
+        <div className="survices">
+            <div className="description">
+                <h2>High <span>quaility</span> services</h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon">
+                            <img src={clock} alt="clock"/>
+                            <h3>Efficient</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={diaphragm} alt="diaphragm"/>
+                            <h3>Diaphragm</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={money} alt="money"/>
+                            <h3>Affordable</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={teamwork} alt="teamwork"/>
+                            <h3>Teamwork</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SurvicesSection;
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutUsSection from "../Components/AboutUsSection";
+import SurvicesSection from "../Components/SurvicesSection"
+
+const Home = () =>{
+    return(
+        <>
+        <AboutUsSection />
+        <SurvicesSection />
+        </>
+    )
+}
+
+export default Home;
